58d25aa2924e6790aaa424de114ca939c140cabc
Samulus <Samulus@users.noreply.github.com>
2015-11-02 00:20:04 -0700

    added game scene stubs

---
+++ src/scene/AudioLag.java
@@ -0,0 +1,10 @@
+package src.scene;
+
+import src.event.VibEvent;
+
+public class AudioLag extends AbstractScene {
+   public void setup() {}
+   public void render() {}
+   public void logic() {}
+   public void input(VibEvent event) {}
+}
---
+++ src/scene/Game.java
@@ -0,0 +1,10 @@
+package src.scene;
+
+import src.event.VibEvent;
+
+public class Game extends AbstractScene {
+   public void setup() {}
+   public void render() {}
+   public void logic() {}
+   public void input(VibEvent event) {}
+}
