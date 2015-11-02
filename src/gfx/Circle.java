a4be659f7a58027abf9dcb1a002a5ddc6b9ee6c8
Samulus <Samulus@users.noreply.github.com>
2015-11-02 00:17:54 -0700

    split Shapes.java into seperate files

---
+++ src/gfx/Circle.java
@@ -0,0 +1,19 @@
+package src.gfx;
+import processing.core.PConstants;
+import src.scene.Scene;
+
+public class Circle extends AbstractShape {
+
+   public Circle(float dst, float maxDistX) {
+     super(dst, maxDistX);
+   }
+
+   public void render() {
+     Scene.p.ellipseMode(PConstants.CORNER);
+     Scene.p.pushMatrix();
+     Scene.p.translate(dst, Scene.p.height/2f - 60, 20);
+     Scene.p.ellipse(Scene.p.random(super.vibrate), Scene.p.random(super.vibrate), 60, 60);
+     Scene.p.popMatrix();
+   }
+
+ }
---
+++ src/gfx/Line.java
@@ -0,0 +1,18 @@
+package src.gfx;
+
+import src.scene.Scene;
+
+public class Line extends AbstractShape {
+   
+   public Line() {
+      super(0.0f, 0.0f);
+   }
+
+   public void render() {
+     Scene.p.pushMatrix();
+     Scene.p.translate(0, Scene.p.height/2f, 20);
+     Scene.p.line(Scene.p.random(super.vibrate), Scene.p.random(super.vibrate), Scene.p.width + Scene.p.random(super.vibrate), Scene.p.random(super.vibrate));
+     Scene.p.popMatrix();
+   }
+
+ }
