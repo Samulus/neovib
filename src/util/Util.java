f14c9737e8ba4ea62f47acd1f3448ea312c43363
Samulus <Samulus@users.noreply.github.com>
2015-11-06 02:55:39 -0700

    whitespace

--- src/util/Util.java
+++ src/util/Util.java
@@ -1,10 +1,10 @@
 package src.util;
 
 public class Util {
-   public static float[] stringToFloat(String[] arr) {
-      float[] output = new float[arr.length];
-      for (int i=0; i < arr.length; ++i)
-        output[i] = Float.parseFloat(arr[i]);
-      return output;
+    public static float[] stringToFloat(String[] arr) {
+        float[] output = new float[arr.length];
+        for (int i = 0; i < arr.length; ++i)
+            output[i] = Float.parseFloat(arr[i]);
+        return output;
     }
 }
