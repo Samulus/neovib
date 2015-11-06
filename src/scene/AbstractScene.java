6b143da71c863e4e2d0aea07e1232571b7f31031
Samulus <Samulus@users.noreply.github.com>
2015-11-06 02:53:08 -0700

    whitespace changes

--- src/scene/AbstractScene.java
+++ src/scene/AbstractScene.java
@@ -1,10 +1,14 @@
 package src.scene;
+
 import src.event.*;
 
-public abstract class AbstractScene { 
+public abstract class AbstractScene {
 
    public abstract void setup();
+
    public abstract void render();
+
    public abstract void logic();
+
    public abstract void input(VibEvent event);
- }
+}
--- src/scene/Gamepad.java
+++ src/scene/Gamepad.java
@@ -3,8 +3,15 @@ package src.scene;
 import src.event.VibEvent;
 
 public class Gamepad extends AbstractScene {
-   public void setup() {}
-   public void render() {}
-   public void logic() {}
-   public void input(VibEvent event) {}
+   public void setup() {
+   }
+
+   public void render() {
+   }
+
+   public void logic() {
+   }
+
+   public void input(VibEvent event) {
+   }
 }
