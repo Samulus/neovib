26f842fd1128d89bbb01b94bcdf57a4205d4fe33
Samulus <Samulus@users.noreply.github.com>
2015-11-06 02:53:43 -0700

    modified to use Browser.fsong for audio playback

--- src/scene/Debug.java
+++ src/scene/Debug.java
@@ -1,51 +1,66 @@
 package src.scene;
 
 import processing.core.PApplet;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+
 import src.audio.Audio;
 import src.audio.Beat;
 import src.audio.Detector;
 import src.event.VibEvent;
 
-// good:
-
-
 public class Debug extends AbstractScene {
 
-   private Audio audio;
-   private Beat beat; 
-   private float size = 32;
+    private Audio audio;
+    private Beat beat;
+    private float size = 32;
 
-   public void setup() {
-      Scene.p.stroke(255);
-      String fpath = "samson.mp3";
-      audio = new Audio(fpath, 1024);
-      ArrayList<Float> blist = Detector.load(fpath);
-      System.out.println(blist.size());// block for a few 1-2 second
-      beat = new Beat(blist);
-      audio.play();
-   }
+    public void setup() {
+        Scene.p.stroke(255);
+        String fpath = null;
 
-   public void render() {
-      Scene.p.background(0);
-      Scene.p.pushMatrix();
-      Scene.p.translate(Scene.p.width / 2f, Scene.p.height / 2f);
-      Scene.p.textSize(this.size);
-      this.size = PApplet.constrain(this.size * 0.95f, 32f, 64f);
-      if (this.size >= 40)
-         Scene.p.text("Beat", Scene.p.random(1), Scene.p.random(1));
-      else
-         Scene.p.text("Beat", 0, 0);
+        if (Browser.fsong == null) {
+            System.out.println("Enter a file first");
+            Scene.p.exit();
+            System.exit(0);
+        }
 
-      Scene.p.popMatrix();
-   }
-   
-   public void logic() {
-      if (this.beat.detect(this.audio.position())) {
-         this.size = 64;
-      }
-   }
 
-   public void input(VibEvent event) {}
+        try {
+            fpath = Browser.fsong.getCanonicalPath();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Debug.java: unable to open file");
+        }
+
+        audio = new Audio(fpath, 1024);
+        System.out.println(fpath);
+        LinkedList<Float> blist = Detector.load(Browser.fsong);
+        System.out.println(blist.size());// block for a few 1-2 second
+        beat = new Beat(blist);
+        audio.play();
+    }
+
+    public void render() {
+        Scene.p.background(0);
+        Scene.p.pushMatrix();
+        Scene.p.translate(Scene.p.width / 2f, Scene.p.height / 2f);
+        Scene.p.textSize(this.size);
+        this.size = PApplet.constrain(this.size * 0.95f, 32f, 64f);
+        if (this.size >= 40)
+            Scene.p.text("Beat", Scene.p.random(1), Scene.p.random(1));
+        else
+            Scene.p.text("Beat", 0, 0);
+
+        Scene.p.popMatrix();
+    }
+
+    public void logic() {
+    }
+
+    public void input(VibEvent event) {
+    }
 
 }
