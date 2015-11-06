4edbcf1b01baae460e6d7917f9dc33cff97b1727
Samulus <Samulus@users.noreply.github.com>
2015-11-06 02:54:49 -0700

    added browser and game modes

--- src/scene/Scene.java
+++ src/scene/Scene.java
@@ -1,54 +1,58 @@
 package src.scene;
 
 import java.util.HashMap;
+
 import processing.core.PApplet;
 import src.event.VibEvent;
 
 public class Scene {
 
-   private static HashMap<String, AbstractScene> list = new HashMap<String, AbstractScene>();
-   private static AbstractScene current = null;
-   public  static PApplet p = null;
+    public static PApplet p = null;
+    private static HashMap<String, AbstractScene> list = new HashMap<String, AbstractScene>();
+    private static AbstractScene current = null;
 
-   public static void setContext(PApplet p) {
-      Scene.p = p;
+    public static void setContext(PApplet p) {
+        Scene.p = p;
 
-   }
-   
-   public static void initScenes() {
-      Scene.p.textFont(Scene.p.createFont("data/ostrich-regular.ttf", 64));
-      Scene.list.put("Title", new Title());
-      Scene.list.put("Debug", new Debug());
-   }
+    }
 
-   public static void focus(String name) {
-      Scene.p.background(0);
-      Scene.current = Scene.list.get(name);
-      Scene.current.setup();
-   }
+    public static void initScenes() {
+        Scene.p.textFont(Scene.p.createFont("data/ostrich-regular.ttf", 64));
+        Scene.list.put("Title", new Title());
+        Scene.list.put("Debug", new Debug());
+        Scene.list.put("Game", new Game());
+        Scene.list.put("Browser", new Browser());
+    }
 
-   public static void render() {
-      if (Scene.current == null)
-         return;
-      Scene.current.render();
-   }
+    public static void focus(VibEvent event) {
+        assert event.isScene();
+        Scene.p.background(0);
+        Scene.current = Scene.list.get(event.getStr());
+        Scene.current.setup();
+    }
 
-   public static void input(VibEvent key) {
-      if (Scene.current == null)
-         return;
-      Scene.current.input(key);
-   }
+    public static void render() {
+        if (Scene.current == null)
+            return;
+        Scene.current.render();
+    }
 
-   public static void logic() {
-      if (Scene.current == null)
-         return;
-      Scene.current.logic();
-   }
-   
-   // Test
-   public static void main(String[] args) {
-      Scene.focus("");
-      
-   }
+    public static void input(VibEvent key) {
+        if (Scene.current == null)
+            return;
+        Scene.current.input(key);
+    }
+
+    public static void logic() {
+        if (Scene.current == null)
+            return;
+        Scene.current.logic();
+    }
+
+    // Test
+    public static void main(String[] args) {
+        //Scene.focus("");
+
+    }
 
 }
