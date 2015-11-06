53de40a761fc244b0bc2f752ace2ee4b1c83f558
Samulus <Samulus@users.noreply.github.com>
2015-11-06 02:55:22 -0700

    modified to use src.ui.Menu;

--- src/scene/Title.java
+++ src/scene/Title.java
@@ -1,67 +1,52 @@
 package src.scene;
+
 import processing.core.PConstants;
+import src.ui.Menu;
 import src.event.*;
 
 public class Title extends AbstractScene {
 
-     private String[] options = {"play", "browse", "video lag", "audio lag", "gamepad", "debug"};
-     private int selected = 0;
+    private String[] options = {"Browse", "Video Lag", "Audio Lag", "Gamepad", "Debug"};
+    private Menu menu;
 
-     public void setup() {
-       Scene.p.noFill();
-       Scene.p.stroke(255);
-       Scene.p.textAlign(PConstants.RIGHT);
-     }
+    public void setup() {
+        menu = new Menu(options, "noscroll");
+        Scene.p.noFill();
+        Scene.p.stroke(255);
+        Scene.p.textAlign(PConstants.RIGHT);
+    }
 
-     public void render() {
-       Scene.p.background(0);
-       renderTitle();
-       renderMenu();
-     }
+    public void render() {
+        Scene.p.background(0);
+        renderTitle();
+        menu.render(Scene.p.width / 7f, Scene.p.height / 2f);
+    }
 
-     public void input(VibEvent event) {
+    public void input(VibEvent event) {
 
-       /* move menu */
-       if (event == VibEvent.INPUT_RIGHT)
-         selected += (selected < options.length-1) ? 1 : 0;
-       else if (event == VibEvent.INPUT_LEFT)
-         selected -= (selected > 0) ? 1 : 0;
+        if (event.isNavigate())
+            menu.navigate(event);
 
        /* switch state */
-       else if (event == VibEvent.INPUT_ACCEPT) {
-          if (options[selected].equals("debug"))
-             EQ.enqueue(VibEvent.SCENE_DEBUG);
-         //Scene.focus("Debug");
-       }
+        if (event == VibEvent.INPUT_ACCEPT) {
+            // I'll change this to EQ.enqueue(VibEvent.lookup(options[menu.getIndex()]) later
+            if (options[menu.getIndex()].equals("Debug")) EQ.enqueue(VibEvent.SCENE_DEBUG);
+            if (options[menu.getIndex()].equals("Browse")) EQ.enqueue(VibEvent.SCENE_BROWSER);
 
-     }
+            //Scene.focus("Debug");
+        }
 
-     public void logic() {}
+    }
 
-     private void renderTitle() {
-       Scene.p.pushMatrix();
-       Scene.p.translate(Scene.p.width/1.1f, Scene.p.height/1.1f);
-       Scene.p.textSize(32);
-       Scene.p.text("neovib", 0, 0);
-       Scene.p.popMatrix();
-     }
+    public void logic() {
+    }
 
-     private void renderMenu() {
-       Scene.p.stroke(255);
-       Scene.p.pushMatrix();
-       Scene.p.translate(Scene.p.width/8f, Scene.p.height/2f);
-       Scene.p.textSize(32);
+    private void renderTitle() {
+        Scene.p.pushMatrix();
+        Scene.p.translate(Scene.p.width / 1.1f, Scene.p.height / 1.1f);
+        Scene.p.textSize(32);
+        Scene.p.text("neovib", 0, 0);
+        Scene.p.popMatrix();
+    }
 
-       for (int i=0; i < this.options.length; ++i) {
-         if (i == selected)
-           Scene.p.text(options[i], Scene.p.random(6), Scene.p.random(6));
-         else
-            Scene.p.text(options[i], 0, 0);
-         Scene.p.translate(128, 0);
-       }
-
-       Scene.p.popMatrix();
-     }
-
-
-   }
+}
