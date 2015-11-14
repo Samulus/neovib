6932269af5e56f2f726d812ea24c071431b7c6ba
Samulus <Samulus@users.noreply.github.com>
2015-11-14 01:01:21 -0700

    initial multithreaded inputcommit

---
+++ src/input/MTInput.java
@@ -0,0 +1,11 @@
+/*
+   MTInput.java
+   -----------------------------------------
+   This class is an attempt to get multithreaded game input working
+   in java.
+
+ */
+package src.input;
+
+public class MTInput {
+}
