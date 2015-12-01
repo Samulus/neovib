6e59e6fa291571140d95219c3588f203cd690b24
Samulus <Samulus@users.noreply.github.com>
2015-11-30 17:45:39 -0700

    added clock

--- src/input/Input.java
+++ src/input/Input.java
@@ -1,52 +1,69 @@
 package src.input;
 
 import processing.core.PConstants;
+import src.clock.Clock;
 import src.event.EQ;
 import src.event.VibEvent;
 
-public class Input extends Thread {
+public class Input {
 
-   private static boolean keyHeld = false;
+    public static boolean HAPPENING = false;
+    private static boolean keyHeld = false;
+    private static Clock time;
+    private static double lastTime;
 
-   public static void poll(char pressed) {
+    public static void poll(char pressed) {
 
-      //if (!Scene.p.keyPressed)
-      //keyHeld = false;
+      /*
+      if (time == null) {
+         time = new Clock();
+         lastTime = time.elapsedTime();
+      } else if (lastTime - time.elapsedTime() < 200) {
+         lastTime = time.elapsedTime();
+      } else return;
+      */
 
-      //if (Scene.p.keyPressed && !keyHeld) {
-      //keyHeld = true;
 
-         switch (pressed) {
+        switch (pressed) {
 
             /* Movement */
             case 'k':
             case PConstants.UP:
-               EQ.enqueue(VibEvent.INPUT_UP);
-               break;
+                EQ.enqueue(VibEvent.INPUT_UP);
+                break;
             case 'j':
             case PConstants.DOWN:
-               EQ.enqueue(VibEvent.INPUT_DOWN);
-               break;
+                EQ.enqueue(VibEvent.INPUT_DOWN);
+                break;
             case 'h':
             case PConstants.LEFT:
-               EQ.enqueue(VibEvent.INPUT_LEFT);
-               break;
+                EQ.enqueue(VibEvent.INPUT_LEFT);
+                break;
             case 'l':
             case PConstants.RIGHT:
-               EQ.enqueue(VibEvent.INPUT_RIGHT);
-               break;
+                EQ.enqueue(VibEvent.INPUT_RIGHT);
+                break;
+            case 'm':
+                EQ.enqueue(VibEvent.INPUT_MARK_DIR);
+                break;
+            case 'q':
+                EQ.enqueue(VibEvent.INPUT_CANCEL);
+                break;
             case PConstants.ENTER:
             case PConstants.RETURN:
-               EQ.enqueue(VibEvent.INPUT_ACCEPT);
-               break;
+                System.out.println("Triggered");
+                EQ.enqueue(VibEvent.INPUT_ACCEPT);
+                break;
             case PConstants.BACKSPACE:
-               EQ.enqueue(VibEvent.INPUT_PREVIOUS);
-               break;
-
-            //case 'h'
-
-         }
+                EQ.enqueue(VibEvent.INPUT_PREVIOUS);
+                break;
+        }
 
 
-   }
+    }
+
+
+    public static void toggle() {
+    }
+
 }
