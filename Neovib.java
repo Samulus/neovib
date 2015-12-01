1fe5449300c684be1aa945ffe0cdddf9fcfdb5b7
Samulus <Samulus@users.noreply.github.com>
2015-11-30 17:50:17 -0700

    misc changes

--- Neovib.java
+++ Neovib.java
@@ -6,48 +6,52 @@ import src.scene.Scene;
 
 public class Neovib extends PApplet {
 
-   public static void main(String[] args) {
-      PApplet.main(Neovib.class.getName());
+    public static void main(String[] args) {
+        PApplet.main(Neovib.class.getName());
 
-   }
+    }
 
-   public void settings() {
-      size(1280, 720, P2D);
-   }
+    public void settings() {
+        size(1280, 720, P2D);
+    }
 
-   public void setup() {
-      Scene.setContext(this);
-      Scene.initScenes();
+    public void setup() {
+        Scene.setContext(this);
+        Scene.initScenes();
+        Scene.p.frameRate(1000);
+        Scene.focus(VibEvent.SCENE_TITLE);
+    }
 
-      Scene.focus(VibEvent.SCENE_TITLE);
-      Scene.p.frameRate(60);
-   }
+    public void draw() {
 
-   public void draw() {
+        while (true) {
 
-      Input.poll(key);
+            //Input.trigger();
+            VibEvent event = EQ.dequeue();
+            if (event == null) break;
 
-      while (true) {
+            if (event.isNavigate()) {
+                Scene.input(event);
+            } else if (event == VibEvent.ERROR_CODE) {
+                System.out.println("error detected audi5k we bounce now");
+                System.exit(-1);
+            } else if (event.isScene()) {
+                Scene.focus(event);
+            }
+        }
 
-         VibEvent event = EQ.dequeue();
-         if (event == null) break;
+        Scene.logic();
+        Scene.render();
 
-         if (event.isNavigate() || event.isTraversal() || event.isPlayer()) {
-            Scene.input(event);
-         } else if (event == VibEvent.ERROR_CODE) {
-            System.out.println("error detected audi5k we bounce now");
-            System.exit(-1);
-         } else if (event.isScene()) {
-            Scene.focus(event);
-         }
-      }
+    }
 
-      Scene.logic();
-      Scene.render();
+    public void keyPressed() {
+        Input.poll(key);
+        //Input.key(key, true);
+    }
 
-   }
-
-   public void keyPressed() {
-   }
+    public void keyReleased() {
+        //Input.key(key, true);
+    }
 
 }
--- src/util/Util.java
+++ src/util/Util.java
@@ -1,23 +1,63 @@
 package src.util;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Util {
 
-   public static float[] stringToFloat(String[] arr) {
-      float[] output = new float[arr.length];
-      for (int i = 0; i < arr.length; ++i)
-         output[i] = Float.parseFloat(arr[i]);
-      return output;
-   }
+    public static float[] stringToFloat(String[] arr) {
+        float[] output = new float[arr.length];
+        for (int i = 0; i < arr.length; ++i)
+            output[i] = Float.parseFloat(arr[i]);
+        return output;
+    }
 
-   public static ArrayList<String> toArrayList(String[] arr) {
+    public static ArrayList<String> toArrayList(String[] arr) {
 
-      ArrayList<String> output = new ArrayList<String>();
-      for (int i = 0; i < arr.length; ++i) {
-         output.add(arr[i]);
-      }
+        ArrayList<String> output = new ArrayList<String>();
+        for (int i = 0; i < arr.length; ++i) {
+            output.add(arr[i]);
+        }
 
-      return output;
-   }
+        return output;
+    }
+
+    // Recursively visit all directories
+    // Borrowed From: http://stackoverflow.com/a/11658192
+    public static Collection<File> listFileTree(File dir) {
+
+        if (dir == null) {
+            System.err.println("MusicDatabase.java: " + dir + "is a nonexistent or invalid directory");
+            return null;
+        }
+
+        Set<File> fileTree = new HashSet<File>();
+        for (File entry : dir.listFiles()) {
+            if (entry.isFile()) fileTree.add(entry);
+            else fileTree.addAll(listFileTree(entry));
+        }
+        return fileTree;
+    }
+
+    /*
+    public ArrayList<String> find(String query) {
+
+        ArrayList<String> results = new ArrayList<String>();
+
+        for (String path : db) {
+            for (String token : path.split("/")) {
+                if (token.toLowerCase().replaceAll("\\s", "").contains(query.toLowerCase().replaceAll("\\s", ""))) { // hahahah
+                    results.add(path);
+                    break; // move on to next path
+                }
+            }
+        }
+
+        return results;
+    }
+    */
+
 }
