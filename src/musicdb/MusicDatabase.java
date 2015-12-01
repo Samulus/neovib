b262ab3c852b474c7f95e7bcc16b14860690f2d9
Samulus <Samulus@users.noreply.github.com>
2015-11-30 17:47:08 -0700

    misc minor changes, this module will be phased out soon

--- src/musicdb/MusicDatabase.java
+++ src/musicdb/MusicDatabase.java
@@ -1,62 +1,42 @@
 package src.musicdb;
 
-import src.clock.Clock;
-
 import java.io.File;
-import java.util.*;
+import java.util.Set;
 
 public class MusicDatabase {
 
-   private ArrayList<String> db;
+    public static File currentSong;
+    public static String currentArtist;
+    public static Set<String> similarArtists;
+    //private ArrayList<String> db
+    public static String libraryPath = "(not set)";
 
-   public MusicDatabase(String fpath) {
-      Collection<File> paths = listFileTree(new File(fpath));
-      db = new ArrayList<String>();
-      for (File f : paths) {
-         db.add(f.toString());
-      }
-      Collections.sort(db);
-   }
+    /*
+    public MusicDatabase(String fpath) {
+        Collection<File> paths = Util.listFileTree(new File(fpath));
+        db = new ArrayList<String>();
+        for (File f : paths) {
+            db.add(f.toString());
+        }
+        Collections.sort(db);
+    }
 
-   // Taken from: http://stackoverflow.com/a/11658192
-   public static Collection<File> listFileTree(File dir) {
+    public static void main(String[] args) {
 
-      Set<File> fileTree = new HashSet<File>();
-      for (File entry : dir.listFiles()) {
-         if (entry.isFile()) fileTree.add(entry);
-         else fileTree.addAll(listFileTree(entry));
-      }
-      return fileTree;
-   }
+        Clock c = new Clock();
 
-   public static void main(String[] args) {
+        String query = "Death Grips";
+        MusicDatabase mdb = new MusicDatabase("/home/sam/music/");
 
-      Clock c = new Clock();
+        if (mdb == null) return;
 
-      String query = "Death Grips";
-      MusicDatabase mdb = new MusicDatabase("/home/sam/music/");
-      for (String s : mdb.find(query)) {
-         System.out.println(s);
-      }
+        for (String s : mdb.find(query)) {
+            System.out.println(s);
+        }
 
-      System.out.println(c.elapsedTime()); // 3-4s initially, 500ms after cacheing
+        System.out.println(c.elapsedTime()); // 3-4s initially, 500ms after cacheing
 
-   }
-
-   public ArrayList<String> find(String query) {
-
-      ArrayList<String> results = new ArrayList<String>();
-
-      for (String path : db) {
-         for (String token : path.split("/")) {
-            if (token.toLowerCase().replaceAll("\\s", "").contains(query.toLowerCase().replaceAll("\\s", ""))) { // hahahah
-               results.add(path);
-               break; // move on to next path
-            }
-         }
-      }
-
-      return results;
-   }
+    }
+    */
 
 }
