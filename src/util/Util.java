42f0b0786c277e89ec4f89a1bd6a832febdc0b07
Samulus <Samulus@users.noreply.github.com>
2015-12-07 00:50:22 -0700

    added misc functions

--- src/util/Util.java
+++ src/util/Util.java
@@ -8,56 +8,53 @@ import java.util.Set;
 
 public class Util {
 
-    public static float[] stringToFloat(String[] arr) {
-        float[] output = new float[arr.length];
-        for (int i = 0; i < arr.length; ++i)
-            output[i] = Float.parseFloat(arr[i]);
-        return output;
-    }
+   public static float[] stringToFloat(String[] arr) {
+      float[] output = new float[arr.length];
+      for (int i = 0; i < arr.length; ++i)
+         output[i] = Float.parseFloat(arr[i]);
+      return output;
+   }
 
-    public static ArrayList<String> toArrayList(String[] arr) {
+   public static ArrayList<String> toArrayList(String[] arr) {
 
-        ArrayList<String> output = new ArrayList<String>();
-        for (int i = 0; i < arr.length; ++i) {
-            output.add(arr[i]);
-        }
+      ArrayList<String> output = new ArrayList<String>();
+      for (int i = 0; i < arr.length; ++i) {
+         output.add(arr[i]);
+      }
 
-        return output;
-    }
+      return output;
+   }
 
-    // Recursively visit all directories
-    // Borrowed From: http://stackoverflow.com/a/11658192
-    public static Collection<File> listFileTree(File dir) {
+   // Recursively visit all directories
+   // Borrowed From: http://stackoverflow.com/a/11658192
+   public static Collection<File> listFileTree(File dir) {
 
-        if (dir == null) {
-            System.err.println("MusicDatabase.java: " + dir + "is a nonexistent or invalid directory");
-            return null;
-        }
+      if (dir == null) {
+         System.err.println("MusicDatabase.java: " + dir + "is a nonexistent or invalid directory");
+         return null;
+      }
 
-        Set<File> fileTree = new HashSet<File>();
-        for (File entry : dir.listFiles()) {
-            if (entry.isFile()) fileTree.add(entry);
-            else fileTree.addAll(listFileTree(entry));
-        }
-        return fileTree;
-    }
+      Set<File> fileTree = new HashSet<File>();
+      for (File entry : dir.listFiles((File pathname) -> !pathname.getName().startsWith("."))) {
+         if (entry.isFile()) fileTree.add(entry);
+         else fileTree.addAll(listFileTree(entry));
+      }
+      return fileTree;
+   }
 
-    /*
-    public ArrayList<String> find(String query) {
+   public static ArrayList<String> find(ArrayList<String> pool, String query) {
 
-        ArrayList<String> results = new ArrayList<String>();
+      ArrayList<String> results = new ArrayList<String>();
 
-        for (String path : db) {
-            for (String token : path.split("/")) {
-                if (token.toLowerCase().replaceAll("\\s", "").contains(query.toLowerCase().replaceAll("\\s", ""))) { // hahahah
-                    results.add(path);
-                    break; // move on to next path
-                }
+      for (String path : pool) {
+         for (String token : path.split("/")) {
+            if (token.toLowerCase().replaceAll("\\s", "").contains(query.toLowerCase().replaceAll("\\s", ""))) { // hahahah
+               results.add(path);
+               break; // move on to next path
             }
-        }
+         }
+      }
 
-        return results;
-    }
-    */
-
+      return results;
+   }
 }
