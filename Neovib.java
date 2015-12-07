1be63c564b7440716c50ef2fc56c0c7da4e20606
Samulus <Samulus@users.noreply.github.com>
2015-12-07 00:53:01 -0700

    modified input mechanism + added Echonest connection stuff

--- Neovib.java
+++ Neovib.java
@@ -2,56 +2,63 @@ import processing.core.PApplet;
 import src.event.EQ;
 import src.event.VibEvent;
 import src.input.Input;
+import src.musicdb.Echonest;
 import src.scene.Scene;
 
 public class Neovib extends PApplet {
 
-    public static void main(String[] args) {
-        PApplet.main(Neovib.class.getName());
 
-    }
+   public static void main(String[] args) {
+      PApplet.main(Neovib.class.getName());
+   }
 
-    public void settings() {
-        size(1280, 720, P2D);
-    }
+   public void settings() {
+      size(1280, 720, P2D);
+   }
 
-    public void setup() {
-        Scene.setContext(this);
-        Scene.initScenes();
-        Scene.p.frameRate(1000);
-        Scene.focus(VibEvent.SCENE_TITLE);
-    }
+   public void setup() {
 
-    public void draw() {
+      /* Graphics / Game Scenes */
+      Scene.setContext(this);
+      Scene.initScenes();
+      Scene.p.frameRate(1000);
+      Scene.focus(VibEvent.SCENE_TITLE);
+      Echonest.ConnectFromFile();
 
-        while (true) {
+      /* Input Subsystem */
+      Input.Init();
+   }
 
-            //Input.trigger();
-            VibEvent event = EQ.dequeue();
-            if (event == null) break;
+   public void draw() {
 
-            if (event.isNavigate()) {
-                Scene.input(event);
-            } else if (event == VibEvent.ERROR_CODE) {
-                System.out.println("error detected audi5k we bounce now");
-                System.exit(-1);
-            } else if (event.isScene()) {
-                Scene.focus(event);
-            }
-        }
+      while (true) {
 
-        Scene.logic();
-        Scene.render();
+         Input.poll();
 
-    }
+         VibEvent event = EQ.dequeue();
+         if (event == null) break;
 
-    public void keyPressed() {
-        Input.poll(key);
-        //Input.key(key, true);
-    }
+         if (event.isNavigate()) {
+            Scene.input(event);
+         } else if (event == VibEvent.ERROR_CODE) {
+            System.out.println("error detected audi5k we bounce now");
+            System.exit(-1);
+         } else if (event.isScene()) {
+            Scene.focus(event);
+         }
+      }
 
-    public void keyReleased() {
-        //Input.key(key, true);
-    }
+      Scene.logic();
+      Scene.render();
+
+   }
+
+   public void keyPressed() {
+      Input.keyAction(key, true);
+   }
+
+   public void keyReleased() {
+      Input.keyAction(key, false);
+   }
 
 }
